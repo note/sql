ef4a46abeb888e9635190386b24f7174222a72e5
Dariusz Mydlarz <dariusz@dariusz-laptop.(none)>
2011-01-21 22:11:06 +0100

    Dodalem widoki dla memberow i filmow; chcialbym jeszcze to troche pomodyfikowac ale brakuje mi przykladowych danych, dzieki ktorym posprawdzam sobie czy wszystko jest ok. Poza tym pozmienialem troche strukture plikow. I proponuje sie tego trzymac. Niech w 'create.sql' siedza same 'creaty' tabeli, w 'generate.sql' dane do naszej bazy, w 'triggers.sql' wszelkie triggery, procedury i widoki a w 'testy.sql' wszystkie rzeczy, ktore NIE niszcza naszej bazy. Moga byc 'inserty' ale pod warunkiem, ze zostana zablokowane, czyli nie wykonaja sie, itd. Po prostu chcialbym, aby 'testy.sql' nie modyfikowal nam juz zawartosci tabel. Poza tym dodalem pole 'labelName', ktorego nam brakowalo. Pozdrawiam ;)

--- testy.sql
+++ testy.sql
@@ -1,120 +1,40 @@
 use OurRental
---obsluga usuwania kategorii
-insert into film(title, director, description)
-values(10, 'Pan Tadeusz0', 'Hoffman', 'Lorem ipsum')
-insert into film(title, director, description)
-values(10, 'Pan Tadeusz1', 'Hoffman', 'Lorem ipsum')
-insert into film(title, director, description)
-values(10, 'Pan Tadeusz2', 'Hoffman', 'Lorem ipsum')
-insert into film(title, director, description)
-values(10, 'Pan Tadeusz3', 'Hoffman', 'Lorem ipsum')
 
 select * from film
-
-insert into category(categoryName)
-values('History0')
-insert into category(categoryName)
-values('History1')
-insert into category(categoryName)
-values('History2')
-
 select * from category
-
-insert into film_and_category(categoryID, filmID)
-values(1,1)
-insert into film_and_category(categoryID, filmID)
-values(1,2)
-insert into film_and_category(categoryID, filmID)
-values(1,3)
-insert into film_and_category(categoryID, filmID)
-values(2,2)
-insert into film_and_category(categoryID, filmID)
-values(2,3)
-
 select * from film_and_category
-
-delete from category where categoryID = 1
-
---obsluga usuwania kategorii dziala poprawnie!
-
---obsluga usuwania memberow
+delete from category where categoryID = 8
 
 --powinno nie zadzialac - za kroki numer telefonu:
-insert into member(lastname, firstname,phone, active)
-values ('Gajecki', 'Marek', '486575122', 1)
+insert into member(lastname, firstname,phone, active) values ('Gajecki', 'Marek', '486575122', 1)
 
-insert into member(lastname, firstname,phone, active)
-values ('Gajecki', 'Marek', '48656575122', 1)
-
-insert into adult(memberID, street, homeNo, city, state, zip, expirationDate)
-values(2, 'Mickiewicza', '30', 'Krakow', 'Malopolska', '30500', GETDATE()+(60*60))
-
-insert into member(lastname, firstname, phone, active)
-values ('Gajecki', 'Michal', '11223344556', 1)
-insert into member(lastname, firstname, phone, active)
-values ('Gajecki', 'MAciek', '11223344556', 1)
-insert into member(lastname, firstname, phone, active)
-values ('Gajecki', 'adam', '11223344556', 1)
-
-insert into juvenile(memberID, birthDate, adult_memberID)
-values(5,GETDATE()-(60*60),1)
-insert into juvenile(memberID, birthDate, adult_memberID)
-values(3,GETDATE()-(60*60),1)
-insert into juvenile(memberID, birthDate, adult_memberID)
-values(4,GETDATE()-(60*60),2)
-
-select * from member
-update member set lastname='reed'
-
-select * from adult a join member m on a.memberID = m.memberID
-select * from juvenile j join member m on j.memberID = m.memberID
-
-exec deleteMember @id=5
-exec deleteMember @id=1
+--Zakomentowalem, bo jak mowila dr Zygmunt, usuwanie memberow to zly pomysl
+--exec deleteMember @id=5
+--exec deleteMember @id=1
 --obsluga usuwania memberow dziala poprawnie!
 
--- powinien jedynie wypisac komunikat:
-delete from juvenile where memberID=1
+select * from member
+select * from adult
+select * from juvenile
 
-select GETDATE()+100
 
 insert into juvenile (memberID, adult_memberID, birthDate) values (3, 7, GETDATE()+100) -- powinno nie zadzialac - birthdate>getdate()
 insert into juvenile (memberID, adult_memberID, birthDate) values (3, 7, '12-11-1877') -- powinno nie zadzialac
-insert into juvenile (memberID, adult_memberID, birthDate) values (3, 7, '12-11-1955') -- powinno spowodowac ustawienie na 0 kolumnny active membera o id=2
-delete from juvenile
-SELECT LEN('COSGG')
 
 insert into member (lastname, firstname, phone, active) values ('Dylan', 'Bob', '1254354354353', 1) -- powinno nie zadzialac - telefon nie ma 11 znakow
-select * from member
 
-insert into medium (mediumName) VALUES ('DVD')
-insert into medium (mediumName) VALUES ('Blu-ray')
-insert into medium (mediumName) VALUES ('VHS')
-
-insert into film (title, director) VALUES ('Chinatown', 'Polanski')
-
-insert into item (filmID, mediumID) VALUES (1, 2)
-
-declare @c int
-set @c=54
-print @c
-
+select * from medium
 select * from item
 select * from loan
 select * from loanhist
+select * from copy
 
-insert into copy (filmID, mediumID, onloan) values (1, 2, 1)
-declare @mul int
-exec insertLoan @copy_Id=100, @member_ID=2, @price_multiplier=@mul output
-select @mul
+--widoki testing
+select * from view_members
+select * from view_films
 
---powinno spowodowac usuniecie z loan i wpisanie do loan hist
-delete from loan where copyID=103
 
-select * from member
-
-exec insertLoan @copy_Id=7, @member_ID=2
-
-select l.copyid, l.memberid, l.outdate from loanhist lh
-		join loan l on l.copyID=lh.copyID
-		where l.memberID=2
+select * from category
+select * from label
+select * from loan
+select * from copy
---
+++ triggers.sql
@@ -0,0 +1,125 @@
+/*	
+ *	PROCEDURES
+ */
+
+create procedure deleteMember @id int
+as
+begin
+	delete from member where memberID in (select memberID from juvenile where adult_memberID=@id)
+	delete from adult where memberID=@id
+	delete from member where memberID=@id
+end
+
+--Returns discount in percentage. If loan can't be insterted then returns -1.
+--Also will not allow to make a loan for one who is keeping films too long
+create procedure insertLoan @copy_id int, @member_id int, @discount int OUTPUT
+as
+begin
+	declare @rowcount int
+	set @discount=0
+	select @rowcount=COUNT(*) from loan where memberID=@member_id
+
+	-- warunek na przetrzymywanie filmow:
+	declare @time_delay int
+	select @time_delay=count(*) from loan where memberID=@member_id and GETDATE() > outDate
+	
+	if(@rowcount<6) and (@time_delay = 0) begin
+		select @rowcount=COUNT(*) from loanhist where memberID=@member_id
+		
+		if(@rowcount>3) begin
+			set @discount=10
+		end
+			
+		insert into loan (copyID, memberID, outDate, dueDate) Values (@copy_id, @member_id, GETDATE(), GETDATE()+4)
+	end
+	else if(@rowcount >= 6) begin
+		set @discount=-1
+		PRINT 'Uzytkownik o id=' + CAST(@member_id as VARCHAR) + ' ma juz wypozyczonych 6 filmow'
+	end
+	else begin
+		PRINT 'Uzytkownik o id=' + CAST(@member_id as VARCHAR) + 'przetrzymuje filmy'
+	end
+end
+
+--will not allow to make a reservation for one who is keeping films too long
+create procedure insertReservation @member_id int, @film_id int, @medium_id int
+as
+begin
+	declare @time_delay int
+	select @time_delay=count(*) from loan where memberID=@member_id and GETDATE() > outDate
+	if (@time_delay > 0) begin
+		PRINT 'Uzytkownik o id=' + CAST(@member_id as VARCHAR) + 'przetrzymuje filmy'
+	end
+	else begin			
+		insert into reservation (memberID, mediumID, filmID, logDate) Values (@member_id, @medium_id, @film_id, GETDATE())
+	end
+end
+
+/*	
+ *	TRIGGERS
+ */
+
+CREATE TRIGGER tr_insertJuvenile
+on juvenile
+after insert
+as
+begin
+	IF DATEDIFF(YEAR, (select birthDate from inserted),  GETDATE())>18  --wazne zalozenie - jak ukonczy rocznikowo 18 lat to uznajemy, ze jest pelnoletni'
+	begin
+		UPDATE member SET active=0 WHERE memberID=(SELECT memberID from inserted)
+	end
+end
+
+CREATE TRIGGER tr_updateJuvenile
+on juvenile
+after update
+as
+begin
+	IF DATEDIFF(YEAR, (SELECT birthDate from inserted),  GETDATE())>18  --wazne zalozenie - jak ukonczy rocznikowo 18 lat to uznajemy, ze jest pelnoletni'
+	begin
+		UPDATE member SET active=0 WHERE memberID=(SELECT memberID from inserted)
+	end
+end
+
+CREATE TRIGGER tr_deleteLoan
+ON loan
+AFTER DELETE
+AS BEGIN
+	INSERT INTO loanhist (outDate, copyID, memberID, dueDate) VALUES ((SELECT outDate from deleted), (SELECT copyID from deleted), (SELECT memberID from deleted), (SELECT dueDate from deleted)) --todo naliczanie kary
+END
+
+/*	
+ *	VIEWS
+ */
+ create view view_members
+ as
+ select M.memberID, M.firstname, M.lastname, M.phone, M.email, A.city, A.street, A.homeNo, A.flatNo, A.zip, A.state, A.expirationDate, null as AdultID,  M.active
+ from member M
+ join adult A on M.memberID = A.memberID
+ union
+ select M.memberID, M.firstname, M.lastname, M.phone, M.email, A.city, A.street, A.homeNo, A.flatNo, A.zip, A.state, J.birthDate, A.memberID as AdultID, M.active
+ from member M
+ join juvenile J on J.memberID = M.memberID
+ join adult A on J.adult_memberID = A.memberID
+ 
+ create view view_films
+ as
+ SELECT F.filmID, F.title
+	,(SELECT CAST(M.mediumName + ', ' AS VARCHAR(MAX))
+		FROM item I
+		join medium M on M.mediumID = I.mediumID
+		WHERE (I.filmID = F.filmID)
+		FOR XML PATH ('')) AS Media
+	,(SELECT CAST(C.categoryName + ', ' AS VARCHAR(MAX))
+		FROM film_and_category FC
+		join category C on C.categoryID = FC.categoryID
+		WHERE (FC.filmID = F.filmID)
+		FOR XML PATH ('')) AS Kategorie
+	,(SELECT CAST(L.labelName + ', ' AS VARCHAR(MAX))
+		FROM film_and_label FL
+		join label L on L.labelID = FL.labelID
+		WHERE (FL.filmID = F.filmID)
+		FOR XML PATH ('')) AS Etykiety
+	,(select count(*) from copy C where C.filmid =  F.filmID) as Kopie
+	,(select COUNT(*) from copy C where C.filmid = F.filmID and C.onLoan = 0) as Dostepnych
+FROM film F
